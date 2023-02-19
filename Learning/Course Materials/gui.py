4acae2df69deb9149a08f365d4effa25730259ed
$enatorP03 <adeiyeprecious650@gmail.com>
2023-02-18 20:05:26 -0800

    Created the PySimpleGui app

--- Learning/Course Materials/gui.py
+++ Learning/Course Materials/gui.py
@@ -5,6 +5,6 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo")
 
-window = sg.Window("My Todo App" , layout= [[label, input_box]])
+window = sg.Window("My Todo App", layout=[[label, input_box]])
 window.read()
 window.close()
